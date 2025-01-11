9741806f8d7de28fa199cf0a64468939568eaa61
David Foweather <david@foweather.us>
2025-01-10 18:06:21 -0600

    Add files via upload

--- chart.js
+++ chart.js
@@ -47,7 +47,7 @@ fetch("http://97.80.237.197:5000/battles/winloss")
     })
     .catch(error => console.error('Error fetching data:', error));
 
-function createClusteredBarChart(chartId, chartTitle, data) {
+function createClusteredBarChart(chartId, chartTitle, data, aspectratio) {
   const labels = data.map(player => player.name); // Player names
   const wins = data.map(player => player.wins);  // Wins
   const losses = data.map(player => player.losses); // Losses
@@ -72,7 +72,7 @@ function createClusteredBarChart(chartId, chartTitle, data) {
       options: {
         responsive: true,
         maintainAspectRatio: true,
-        aspectRatio: 3,
+        aspectRatio: aspectratio,
           plugins: {
               title: {
                   display: true,
@@ -95,12 +95,67 @@ function createClusteredBarChart(chartId, chartTitle, data) {
 fetch("http://97.80.237.197:5000/battles/teammates")
     .then(response => response.json())
     .then(data => {
-        createClusteredBarChart('teammatesChart', 'Most Winning Teammates', data);
+        createClusteredBarChart('teammatesChart', 'Most Winning Teammates', data, 3);
     });
 
 // Fetch opponents data
 fetch("http://97.80.237.197:5000/battles/opponents")
     .then(response => response.json())
     .then(data => {
-        createClusteredBarChart('opponentsChart', 'Biggest Rivals', data);
+        createClusteredBarChart('opponentsChart', 'Biggest Rivals', data, 2);
     });
+
+// Function to render the chart
+function renderChart(data) {
+const labels = data.map(row => row[0]); // Extract mode names
+const wins = data.map(row => row[1]);  // Extract win counts
+const losses = data.map(row => row[2]); // Extract loss counts
+console.log(labels,wins,losses)
+new Chart(document.getElementById('modesChart').getContext('2d'), {
+    type: 'bar',
+    data: {
+    labels: labels,
+    datasets: [
+        {
+        label: 'Wins',
+        backgroundColor: 'green',
+        data: wins
+        },
+        {
+        label: 'Losses',
+        backgroundColor: 'red',
+        data: losses
+        }
+    ]
+    },
+    options: {
+    responsive: true,
+    scales: {
+        x: {
+        beginAtZero: true
+        },
+        y: {
+        beginAtZero: true
+        }
+    }
+    }
+});
+}
+
+
+
+fetch('http://97.80.237.197:5000/gamemodes')
+.then(response => {
+    // Ensure the response is successful
+    if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+})
+.then(data => {
+    console.log('API Data:', data);
+    renderChart(data);
+})
+.catch(error => {
+    console.error('Error fetching data:', error);
+});
